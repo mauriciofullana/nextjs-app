ad28f14d47ee1b3ec5809a7f5263b114d3f9a6d2
Mauricio Fullana <mfullana4@gmail.com>
2022-01-23 14:39:39 -0300

    Adding filter control and navigation to filter page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,24 @@
+import { Fragment } from "react";
+import { useRoute, useRouter } from "next/router";
 import EventList from "../../components/events/EventList";
+import EventsSearch from "../../components/events/EventsSearch";
 import { getAllEvents } from "../../dummy-data";
 
 const EventsPage = () => {
+  const router = useRouter();
   const events = getAllEvents();
 
-  return <EventList items={events} />;
+  const handleFindEvents = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
+
+  return (
+    <Fragment>
+      <EventsSearch onSearch={handleFindEvents} />
+      <EventList items={events} />;
+    </Fragment>
+  );
 };
 
 export default EventsPage;
