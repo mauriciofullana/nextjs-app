c1e121270b9a2ef49667ba71332a2fc1a9f4bfcb
Mauricio Fullana <mfullana4@gmail.com>
2022-01-23 21:03:24 -0300

    Filter page logic

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,8 +1,28 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import EventList from "../../components/events/EventList";
+import { getFilteredEvents } from "../../dummy-data";
+
 const FilteredEventPage = () => {
+  const router = useRouter();
+  const slug = router.query.slug;
+  if (!slug) {
+    return <p className="center">Loading...</p>;
+  }
+  const filter = {
+    year: +slug[0],
+    month: +slug[1],
+  };
+  const events = getFilteredEvents(filter);
+
+  if (!events || events.length === 0) {
+    return <p>No events found.</p>;
+  }
+
   return (
-    <div>
-      <h1>Filtered event page</h1>
-    </div>
+    <Fragment>
+      <EventList items={events} />
+    </Fragment>
   );
 };
 
