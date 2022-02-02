67aa9283e1d8ef1edb4a8153b737ae84f1d15e54
Mauricio Fullana <mfullana4@gmail.com>
2022-02-02 00:31:17 -0300

    Optimization - change img html to Image Next component

--- components/event-detail/EventLogistics.js
+++ components/event-detail/EventLogistics.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import AddressIcon from "../icons/address-icon";
 import DateIcon from "../icons/date-icon";
 import LogisticsItem from "./LogisticsItem";
@@ -16,7 +17,7 @@ const EventLogistics = (props) => {
   return (
     <section className={classes.logistics}>
       <div className={classes.image}>
-        <img src={`/${image}`} alt={imageAlt} />
+        <Image src={`/${image}`} alt={imageAlt} width={400} height={400} />
       </div>
       <ul className={classes.list}>
         <LogisticsItem icon={DateIcon}>
--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Button from "../ui/Button";
 import classes from "./event-item.module.css";
 import DateIcon from "../icons/date-icon";
@@ -17,7 +18,7 @@ const EventItem = (props) => {
 
   return (
     <li className={classes.item}>
-      <img src={`/${image}`} alt={title} />
+      <Image src={`/${image}`} alt={title} width={250} height={160} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
