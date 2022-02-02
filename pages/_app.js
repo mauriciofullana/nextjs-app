5f84b7fee4cde459400d2a04f81b5f3808f382ff
Mauricio Fullana <mfullana4@gmail.com>
2022-02-02 00:09:14 -0300

    Optimization - next/head component in _app.js to add meta and title to all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
+import Head from 'next/head';
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>NEXT JS</title>
+        <meta name='description' content='NextJS Events' />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
