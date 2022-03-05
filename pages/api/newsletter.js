422db842097e064f2c3b0117fed8a618771b8458
Mauricio Fullana <mfullana4@gmail.com>
2022-03-05 18:43:34 -0300

    Adding mongodb connection to email registration.

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,14 +1,23 @@
-function handler(req, res) {
-  if (req.method === "POST") {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
+  if (req.method === 'POST') {
     const userEmail = req.body.email;
 
-    if (!userEmail || !userEmail.includes("@")) {
-      res.status(422).json({ message: "Invalid email address." });
+    if (!userEmail || !userEmail.includes('@')) {
+      res.status(422).json({ message: 'Invalid email address.' });
       return;
     }
 
-    console.log(userEmail);
-    res.status(201).json({ message: "Signed up" });
+    // adding email to mongodb database
+    const client = await MongoClient.connect(
+      'mongodb+srv://next-user:ZiJ1Ui22z9k17iTZ@cluster0.j9t0v.mongodb.net/newsletter?retryWrites=true&w=majority'
+    );
+    const db = client.db();
+    await db.collection('emails').insertOne({ email: userEmail });
+    client.close();
+
+    res.status(201).json({ message: 'Signed up' });
   }
 }
 
